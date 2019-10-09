8a70dc4ea66cc95cc037936741cbdb3fac30f2e1
nishantnikhil-github <54797587+nishantnikhil-github@users.noreply.github.com>
2019-10-09 17:14:39 +0200

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -3,7 +3,7 @@ $(aws ecr get-login --no-include-email --region eu-west-2)
 for docker in $(find ./ -name Dockerfile); do
   name=$(dirname $docker)
   echo ${name#./}
-  (cd $name && docker build -t ${name#./} .)
+  (cd $name && docker build -t platform-builder/${name#./} .)
   docker tag ${name#./}:latest 090107652998.dkr.ecr.eu-west-2.amazonaws.com/platform-builder/${name#./}:latest
   docker push 090107652998.dkr.ecr.eu-west-2.amazonaws.com/platform-builder/${name#./}:latest
 done
