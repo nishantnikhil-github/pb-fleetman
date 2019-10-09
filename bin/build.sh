45e25bfe9485fe7ce7078b739f21ecb6e8f0edc6
nishantnikhil-github <54797587+nishantnikhil-github@users.noreply.github.com>
2019-10-09 16:20:38 +0200

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -4,6 +4,6 @@ for docker in $(find ./ -name Dockerfile); do
   name=$(dirname $docker)
   echo ${name#./}
   (cd $name && docker build -t ${name#./} .)
-  docker tag ${name#./}:latest 090107652998.dkr.ecr.eu-west-2.amazonaws.com/platform-builder:latest
+  docker tag ${name#./}:latest 090107652998.dkr.ecr.eu-west-2.amazonaws.com/${name#./}:latest
   docker push 090107652998.dkr.ecr.eu-west-2.amazonaws.com/${name#./}:latest
 done
