3355a05e2889df64b6bbac17dd0369bb701cd499
nishantnikhil-github <54797587+nishantnikhil-github@users.noreply.github.com>
2019-10-10 12:06:11 +0200

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -3,7 +3,7 @@ for docker in $(find ./ -name Dockerfile); do
   name=$(dirname $docker)
   echo ${name#./}
   (cd $name && docker build -t ${name#./} .)
-  aws ecr create-repository --repository-name ${name#./}
+  aws ecr create-repository --repository-name ${name#./} --image-tag-mutability IMMUTABLE
   $(aws ecr get-login --no-include-email --region eu-west-2)
   docker tag ${name#./}:latest 090107652998.dkr.ecr.eu-west-2.amazonaws.com/${name#./}:latest
   docker push 090107652998.dkr.ecr.eu-west-2.amazonaws.com/${name#./}:latest
