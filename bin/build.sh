31f12ecb2eb1378be7cd1c0b94f5f964aa08eb81
nishantnikhil-github <54797587+nishantnikhil-github@users.noreply.github.com>
2019-10-10 08:42:26 +0200

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -3,6 +3,7 @@ for docker in $(find ./ -name Dockerfile); do
   name=$(dirname $docker)
   echo ${name#./}
   (cd $name && docker build -t ${name#./} .)
-  docker tag ${name#./}:latest 090107652998.dkr.ecr.eu-west-2.amazonaws.com/platform-builder/
-  docker push 090107652998.dkr.ecr.eu-west-2.amazonaws.com/platform-builder/${name#./}
+  aws ecr create-repository --repository-name ${name#./}
+  docker tag ${name#./}:latest 090107652998.dkr.ecr.eu-west-2.amazonaws.com/${name#./}:latest
+  docker push 090107652998.dkr.ecr.eu-west-2.amazonaws.com/${name#./}:latest
 done
